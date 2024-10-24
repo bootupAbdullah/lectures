4418d216b2ed33f2562ddc43fc5c12f0398930b1
Abdulla Durrani <Abdullahkd@gmail.com>
2024-10-23 21:17:09 -0400

    completed dom lecture videos

---
+++ dom/dom-events/js/app.js
@@ -0,0 +1,23 @@
+const likeButtonElement = document.querySelector('#like-button');
+
+
+likeButtonElement.addEventListener('click', ()=> {
+    console.log('You clicked me!');
+})
+
+
+const commentButtonElement = document.querySelector('#comment-button');
+const commentListElement = document.querySelector('ul');
+const inputElement = document.querySelector('input')
+
+commentButtonElement.addEventListener('click', ()=> {
+    if(inputElement.value != "" ) {
+    const commentElement = document.createElement('li');
+    commentElement.textContent = inputElement.value
+    commentListElement.appendChild(commentElement);
+    inputElement.value = "";
+}   
+
+})
+
+console.dir(inputElement)
