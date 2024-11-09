8b4095cdb5e1f4766e230aa7988e90784fd57cbd
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-09 12:54:21 -0500

    ejs video lectures

--- unit-2/ejs/server.js
+++ unit-2/ejs/server.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 
 const inventory = [
-    { name: 'Candle', qty: 4 },
+  { name: 'Candle', qty: 4 },
   { name: 'Cheese', qty: 10 },
   { name: 'Phone', qty: 1 },
   { name: 'Tent', qty: 0 },
   { name: 'Torch', qty: 5 }
 ];
 
+// route handler
 app.get('/', (req, res) => {
   res.render('home.ejs', {
     msg: 'Here is your inventory',
@@ -23,13 +24,11 @@ app.get('/', (req, res) => {
 
 //creating route for line items
 app.get('/:itemId', (req, res) => {
-    const index = req.params.itemId
-    res.render('show.ejs', {
-        item: inventory[index]
-    })
+  const index = req.params.itemId
+  res.render('show.ejs', {
+    item: inventory[index]
+  })
 });
-
-
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
