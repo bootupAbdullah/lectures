cab539486b8d958aaa957eb797b2b63d579d8e22
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-09 11:32:29 -0500

    added unit dir

---
+++ unit-2/ejs/server.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require('express');
+const app = express();
+
+const inventory = [
+    { name: 'Candle', qty: 4 },
+  { name: 'Cheese', qty: 10 },
+  { name: 'Phone', qty: 1 },
+  { name: 'Tent', qty: 0 },
+  { name: 'Torch', qty: 5 }
+];
+
+app.get('/', (req, res) => {
+  res.render('home.ejs', {
+    msg: 'Here is your inventory',
+    player: {
+        name: "The Lord Jesus Christ"
+    },
+    inventory: inventory
+  });
+});
+
+//creating route for line items
+app.get('/:itemId', (req, res) => {
+    const index = req.params.itemId
+    res.render('show.ejs', {
+        item: inventory[index]
+    })
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
