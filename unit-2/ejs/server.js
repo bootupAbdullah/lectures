5c26bc8cf0b873f1bdb4c9a3a1e895041ac5c1aa
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-11 22:03:36 -0500

    workingcd .. ls

--- unit-2/ejs/server.js
+++ unit-2/ejs/server.js
@@ -30,6 +30,6 @@ app.get('/:itemId', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
 });
