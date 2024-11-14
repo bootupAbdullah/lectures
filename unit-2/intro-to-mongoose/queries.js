e275c979a9c3f15cb1c1ee6d2eb5dd371009fd6b
Abdulla Durrani <Abdullahkd@gmail.com>
2024-11-14 14:03:18 -0500

    initial commit

--- unit-2/intro-to-mongoose/queries.js
+++ unit-2/intro-to-mongoose/queries.js
@@ -2,29 +2,22 @@
 
 const dotenv = require('dotenv');
 dotenv.config();
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+console.log(process.env.MONGODB_URI)
+
+const Todo = require('./models/todo.js')
 
 const connect = async () => {
-  // Connect to MongoDB using the MONGODB_URI specified in our .env file.
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log('Connected to MongoDB');
+  await mongoose.connect(process.env.MONGODB_URI)
+  console.log('Connected to MongoDB')
 
-  // Call the runQueries function, which will eventually hold functions to work
-  // with data in our db.
-  await runQueries()
+  const todoData = {
+    text: 'Learn JS',
+    isComplete: false
+  }
 
-  // Disconnect our app from MongoDB after our queries run.
-  await mongoose.disconnect();
-  console.log('Disconnected from MongoDB');
-
-  // Close our app, bringing us back to the command line.
-  process.exit();
-};
-
-const runQueries = async () => {
-  console.log('Queries running.')
-  // The functions calls to run queries in our db will go here as we write them.
-};
+  await Todo.create(todoData)
+}
 
 connect()
 /*------------------------------ Query Functions -----------------------------*/
