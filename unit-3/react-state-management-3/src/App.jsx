534694f7c97e3b315de6043d7c63d0e148898e8a
Abdulla Durrani <Abdullahkd@gmail.com>
2024-12-11 10:24:02 -0500

    stopped on usetate hook

--- unit-3/react-state-management-3/src/App.jsx
+++ unit-3/react-state-management-3/src/App.jsx
@@ -1,8 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [myInfo, setMyInfo] = useState([
+    {
+      firstName: "Abdullah",
+      lastName: "Durrani",
+      hasPets: true,
+      age: 36
+    }
+  ])
+  
+  console.log(isDarkMode)
+  console.log(myInfo[0])
+
+
   return (
-    <h1>Hello world!</h1>
+    <div className={isDarkMode ? 'dark' : 'light'}>
+      <h1>Hello world!</h1>
+      <p>Hello, my name is {myInfo[0].firstName} {myInfo[0].lastName}, I am {myInfo[0].age} years old, and I {myInfo[0].hasPets ? 'have' : 'don\'t have'} pets. </p>
+    </div>
   );
 };
 
